26b2657de59d20f91ee87aa74a84b9e1b0a0a9a7
harofax <daniel.kh555@gmail.com>
2019-10-11 00:49:12 +0200

    försöker fixa allt kaos hjälp

--- labb6/jukebox.py
+++ labb6/jukebox.py
@@ -1,44 +1,36 @@
 import timeit
 from random import shuffle
+from labb6.song import Song
 
 #########################################################
 #                        SETUP                          #
 #########################################################
 
-song_list = []
-
-class Song:
-    def __init__(self, trackID, songID, artist, song_title):
-        self.trackID = trackID
-        self.songID = songID
-        self.artist = artist
-        self.song_title = song_title
-
-    def __str__(self):
-        return "artist: " + self.artist + ", song_title: " + self.song_title
-
-    def __lt__(self, other):
-        return ((self.artist) < (other.artist))
 
 
-with open("unique_tracks.txt", "r", encoding="utf-8") as tracklist:
-    for row in tracklist:
-        songinfo = row.split("<SEP>")
-        track_ID = songinfo[0]
-        song_ID = songinfo[1]
-        artist = songinfo[2]
-        song_title = songinfo[3]
-        song = Song(track_ID, song_ID, artist, song_title)
-        song_list.append(song)
+def readfile(filename):
+    song_list = []
+    with open(filename, "r", encoding="utf-8") as tracklist:
+        for row in tracklist:
+            songinfo = row.split("<SEP>")
+            track_ID = songinfo[0]
+            song_ID = songinfo[1]
+            artist = songinfo[2]
+            song_title = songinfo[3]
+            song = Song(track_ID, song_ID, artist, song_title)
+            song_list.append(song)
+
+    return song_list
+
 
 #########################################################
 #                       SÖKNING                         #
 #########################################################
 
 def linear_search(tracklist, artist):
-    for song in tracklist:
-        if song.artist == artist:
-            return True
+    for i in range(len(tracklist)):
+        if tracklist[i].artist == artist:
+            return tracklist[i]
     return False
 
 
@@ -109,23 +101,27 @@ def quicksort(array):
 
 def main():
 
-    n = 1000
+    song_list = readfile("unique_tracks.txt")
+
+    n = 10000
     smaller_list = song_list[0:250000]
 
     size = len(smaller_list)
     print("Antal element:", size)
 
-    sista = song_list[size - 1]
+    sista = smaller_list[size - 2]
     test_artist = sista.artist
+
+    print(str(sista))
 
     song_dict = {song.artist: song for song in smaller_list}
 
     linjtid = timeit.timeit(stmt = lambda: linear_search(smaller_list, test_artist), number =n)
     print("linjärsökningen tog", round(linjtid, 4), "sekunder.")
 
-    smaller_list.sort()
+    sorted_songs = sorted(smaller_list)
 
-    bin_time = timeit.timeit(stmt = lambda: binary_search(smaller_list, test_artist), number =n)
+    bin_time = timeit.timeit(stmt = lambda: binary_search(sorted_songs, test_artist), number =n)
     print("Binärsökningen på sorterad lista tog", round(bin_time, 4), "sekunder.")
 
     hash_time = timeit.timeit(stmt = lambda: hash_search(song_dict, test_artist), number =n)
@@ -150,11 +146,11 @@ main()
 #################################################################
 #            | n= 250 000         n= 500 000     n= 1 000 000   #
 # ___________|_________________________________________________ #
-# Linjär     |     13.1193           24.1981       48.5282      #
+# Linjär     |    1.3768             13.6894       48.5282      #
 #            |                                                  #
-# Binär      |     2.487             2.29683       5.9332       #
+# Binär      |    0.0115             0.0091       5.9332       #
 #            |                                                  #
-# Dict       |     0.0586            0.1102        0.2279       #
+# Dict       |    0.0002             0.0001        0.2279       #
 #            |                                            [sek] #
 #################################################################
 
